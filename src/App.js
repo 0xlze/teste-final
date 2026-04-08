5b56cc35a8d22747cd7bec18130defd693872cb1
Luiz Eduardo <luiz_gss@hotmail.com>
2026-04-08 12:31:06 -0300

    Refactor App component with GlobalStyle and alt text
    Added alt attribute to image and adjusted JSX structure.

--- src/App.js
+++ src/App.js
@@ -3,46 +3,50 @@ import './App.css';
 import { createGlobalStyle } from 'styled-components'
 import { Header } from './components/Header';
 
-
 function App() {
   return (
-    <div className="container">
-      <Header />
-      <main>
-        <div className='display'>
-          <img src={vulpix} />
-          <h3>Name</h3>
-          <h3>Type</h3>
-          <button>More</button>
-        </div>
-
-        <div className='lista'>
-          <div className='pokeSearch'>
-            <input type="text"
-              placeholder='Digite o Pokemon ou o Tipo...'
-            />
+    <>
+      <GlobalStyle />
+      <div className="container">
+        <Header />
+        <main>
+          <div className='display'>
+            {/* Correção 1: Adicionado o atributo alt */}
+            <img src={vulpix} alt="Vulpix" /> 
+            <h3>Name</h3>
+            <h3>Type</h3>
+            <button>More</button>
           </div>
-          <ul>
-            <li>#037 Vulpix</li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-          <button>Veja mais</button>
-        </div>
-      </main>
-    </div>
+
+          <div className='lista'>
+            <div className='pokeSearch'>
+              <input type="text"
+                placeholder='Digite o Pokemon ou o Tipo...'
+              />
+            </div>
+            <ul>
+              <li>#037 Vulpix</li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+            <button>Veja mais</button>
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
 
+// Correção 2: Removido o ponto final extra após 16px
 const GlobalStyle = createGlobalStyle`
 * {
   margin: 0;
@@ -50,12 +54,11 @@ const GlobalStyle = createGlobalStyle`
   box-sizing: border-box;
   color: rgb(30, 30, 30);
   font-family: 'Jockey One', sans-serif;
-  font-size: 16px;.
+  font-size: 16px;
 }
 
 li {
   list-style: none;
-  
 }
 `
 
